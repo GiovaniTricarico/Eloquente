e0780fc8aa98b34e9bc4acfc4ee31cfe2a6131fa
Giovani Tricarico <giovani@ejcm.com.br>
2018-08-02 09:29:57 -0300

    first commit

--- app/Aluno.php
+++ app/Aluno.php
@@ -23,14 +23,29 @@ class Aluno extends Model
 
 
     public function createAluno($request){
-
+        $aluno_novo = new Aluno; //instancia a model
+        
+        $aluno_novo->nome = $request->nome; //atribui o nome
+        $aluno_novo->faltas = $request->faltas; // atribui faltas
+        $aluno_novo->media = $request->media; // atribui media
+        $aluno_novo->turma = $request->turma; //atribui turma
+        $aluno_novo->serie = $request->serie; //atribui serie
+        $aluno_noco->registro = $request->registro; //atribui registro
+        
+        $aluno_novo->save(); //salva o registro
     }
 
     public function updateAluno($request, $id){
+        $aluno = App\Aluno::find(23);
 
+        $aluno->media = 5.0;
+        $aluno->status = "recuperacao";
+        $aluno->save();
     }
 
     public function deleteAluno($id){
+        //$ex_aluno = App\Aluno::find(1);
+        $ex_aluno->delete();
 
     }
 
@@ -43,19 +58,36 @@ class Aluno extends Model
     //     }
     //     return $ordenados;
     // }
+    
+    public function alteraAluno($request){
+        
+        if(!$this->registro){
+            $this->registro = $request->registro;
+        }
 
-    // public function alteraAluno($request){
-        
-    //     if(!$this->registro){
-    //         $this->registro = $request->registro;
-    //     }
-    //     $this->nome = $request->nome;
-    //     $this->turma = $request->turma;
-    //     $this->serie = $request->serie;
-    //     $this->media = $request->media;
-    //     $this->faltas = $request->faltas;
-        
-    //     $this->save();
-    // }
+        $this->nome = $request->nome;
+        $this->turma = $request->turma;
+        $this->serie = $request->serie;
+        $this->media = $request->media;
+        $this->faltas = $request->faltas;
+
+        if ($request->media > 6.9){
+            $this->status = "Aprovado";
+        }
+
+        if ($request->media < 5.0){
+            $this->status = "Reprovado";
+        }
+
+        if (($request->media < 6.9) && ($request->media > 4.9)){
+            $this->status = "Prova Final";
+        }
+        //$this->status = $this->status($request->nota);
+
+
+        $this->save();
+    }
+
+
 
 }
--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -13,7 +13,7 @@ class AlunoController extends Controller
     public function index()
     {
         $alunos = Aluno::all();
-        // $alunos = Aluno::withTrashed()->get();
+        $alunos = Aluno::withTrashed()->get();
         
         return view('alunos',['alunos' => $alunos]);
     }
@@ -21,18 +21,25 @@ class AlunoController extends Controller
 
     public function insereAluno(Request $request)
     {
+        $aluno = new Aluno;
+        $aluno->alteraAluno($request);
         return back();
     }
 
 
     public function atualizaAluno(Request $request, $id)
     {
-       return back();
+        $aluno = Aluno::find($id);
+        $aluno->alteraAluno($request);
+        return back();
     }
 
     public function deletaAluno($id)
     {
-       return back();
+        Aluno::destroy($id);
+        //$ex_aluno = Aluno::find($id);
+        //$ex_aluno->delete();
+        return back();
     }
 
 
